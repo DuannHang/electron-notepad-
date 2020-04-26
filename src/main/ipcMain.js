8577f208c09a0011f80dfb55741068eb3a8da7a7
DuannHang <2251821131@qq.com>
2020-04-26 21:52:47 +0800

    1·系统托盘 2·系统图标闪烁 3·系统托盘的右键、双击事件 4·系统图标的名字tooltip

--- src/main/ipcMain.js
+++ src/main/ipcMain.js
@@ -1,5 +1,5 @@
-var { Menu, shell, ipcMain, BrowserWindow, app } = require('electron');
-
+var { Menu, shell, ipcMain, BrowserWindow, app, Tray } = require('electron');
+var path = require('path')
 
 var template = [{
         label: '文件',
@@ -144,8 +144,74 @@ Menu.setApplicationMenu(m)
 
 
 
+// 系统托盘
+var iconTray = new Tray(path.join(__dirname, '../static/img/666.png'))
+var templateTray = [{
+        label: '火锅',
+        click() {
+            // 通过默认浏览器打开
+            alert('吃个火锅')
+        }
+    },
+    {
+        label: '串串',
+        click() {
+            // 通过默认浏览器打开
+            alert('吃个串串')
+        }
+    },
+    {
+        label: '帮助',
+        click() {
+            // 通过默认浏览器打开
+            shell.openExternal('https://www.baidu.com/');
+
+        }
+    },
+    {
+        label: '退出',
+        accelerator: 'Shift+Esc',
+        click: function() {
+            //退出--关闭系统
+            // app.quit()
+            // 还要提示用户保存更改文件--这个时候，需要主进程与渲染进程通信，
+            // 点击退出时，广播给渲染进程，拿到这个消息，检查渲染进程里是否未保存，
+            BrowserWindow.getFocusedWindow().webContents.send('action', 'exit')
+        }
+    }
+]
+var iconTrayMenu = Menu.buildFromTemplate(templateTray)
+    // 绑定托盘菜单
+iconTray.setContextMenu(iconTrayMenu)
+iconTray.setToolTip('duanhangNotePad')
 
 
+
+//实现点击关闭按钮让应用保存在托盘里面 ，双击托盘打开应用
+var win = BrowserWindow.getFocusedWindow()
+win.on('close', (e) => {
+        if (!win.isFocused()) {
+            win = null
+        } else {
+            //阻止窗口的关闭事件
+            e.preventDefault()
+            win.hide()
+        }
+    })
+    //监听任务栏图标的点击事件
+iconTray.on('double-click', function() {
+        win.show()
+    })
+    //闪烁图标
+var count = 0
+var timer = setInterval(() => {
+    count++
+    if (count % 2 === 0) {
+        iconTray.setImage(path.join(__dirname, '../static/img/666.png'))
+    } else {
+        iconTray.setImage(path.join(__dirname, '../static/img/empty.png'))
+    }
+}, 500);
 
 
 // 右键菜单
